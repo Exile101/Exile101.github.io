7967704f60a4258dd6cb5df1ce3724e0d08049ad
Exile <sigma.6.2009@gmail.com>
2015-05-11 03:02:26 +0530

    Adding anniversary website

--- Year/js/script.js
+++ Year/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	var n=100; // number of bloks 
 	var target=$('#bloks');
-	var bullet="<div class='blok' style='background:#000;margin:1px;'></div>";
+	var bullet="<div class='blok'></div>";
 	for (var i =1; i <= n; i++) {
 	 	target.append(bullet);
 	 }; 
