63e650fd782b3100f9ab958de9886c0db4f47e5a
Suman Senapati <sigma.6.2009@gmail.com>
2015-06-29 09:20:43 +0530

    AppolysusV2:initial commit

---
+++ AppolysusV2/js/script.js
@@ -0,0 +1,26 @@
+$(window).load(function(){
+
+
+	// horwheel init
+	var wrapper = document.querySelector('.viewer');
+
+	horwheel(wrapper);
+
+
+	// on hover effects
+	$('.c_overlay').mouseover(function(){
+		$(this).siblings().css("-webkit-transform","scale(1.1)");
+		$(this).siblings().css("transform","scale(1.1)");
+		$(this).siblings().css("-moz-transform","scale(1.1)");
+		$(this).siblings().css("-o-transform","scale(1.1)");
+	});
+
+	$('.c_overlay').mouseout(function(){
+		$(this).siblings().css("-webkit-transform","scale(1)");
+		$(this).siblings().css("transform","scale(1)");
+		$(this).siblings().css("-moz-transform","scale(1)");
+		$(this).siblings().css("-o-transform","scale(1)");
+	});
+
+
+})
