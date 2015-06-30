73fe68fc97a03a8a6aac748c90ea3254a40d31a8
Suman Senapati <sigma.6.2009@gmail.com>
2015-06-30 11:28:28 +0530

    removed redundant images and resized images

--- AppolysusV2/js/script.js
+++ AppolysusV2/js/script.js
@@ -23,22 +23,30 @@ $(window).load(function(){
 
 	// thumbs navigation
 	$('.t_overlay').eq(0).click(function(){
-		$('.viewer').animate({scrollLeft:0},1800);
+		$('.viewer').animate({scrollLeft:0},1800,'easeInQuad');
 	});
 
 	$('.t_overlay').eq(1).click(function(){
-		$('.viewer').animate({scrollLeft:1000},1800);
+		$('.viewer').animate({scrollLeft:1000},1800,'easeInQuad');
 	});
 
 	$('.t_overlay').eq(2).click(function(){
-		$('.viewer').animate({scrollLeft:2000},1800);
+		$('.viewer').animate({scrollLeft:2000},1800,'easeInQuad');
 	});
 
 	$('.t_overlay').eq(3).click(function(){
-		$('.viewer').animate({scrollLeft:3000},1800);
+		$('.viewer').animate({scrollLeft:3000},1800,'easeInQuad');
 	});
 
 	$('.t_overlay').eq(4).click(function(){
-		$('.viewer').animate({scrollLeft:4000},1800);
+		$('.viewer').animate({scrollLeft:4000},1800,'easeInQuad');
+	});
+
+	$('.t_overlay').eq(5).click(function(){
+		$('.viewer').animate({scrollLeft:5000},1800,'easeInQuad');
+	});
+
+	$('.t_overlay').eq(6).click(function(){
+		$('.viewer').animate({scrollLeft:6000},1800,'easeInQuad');
 	});
 })
