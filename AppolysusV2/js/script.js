405da8805cd37def83ee1320e1e32e80bc8d744b
Suman Senapati <sigma.6.2009@gmail.com>
2015-06-30 09:33:23 +0530

    horizontal scroll added

--- AppolysusV2/js/script.js
+++ AppolysusV2/js/script.js
@@ -4,10 +4,10 @@ $(window).load(function(){
 
 	// on hover effects
 	$('.c_overlay').mouseover(function(){
-		$(this).siblings().css("-webkit-transform","scale(1.1)");
-		$(this).siblings().css("transform","scale(1.1)");
-		$(this).siblings().css("-moz-transform","scale(1.1)");
-		$(this).siblings().css("-o-transform","scale(1.1)");
+		$(this).siblings().css("-webkit-transform","scale(1.2)");
+		$(this).siblings().css("transform","scale(1.2)");
+		$(this).siblings().css("-moz-transform","scale(1.2)");
+		$(this).siblings().css("-o-transform","scale(1.2)");
 	});
 
 	$('.c_overlay').mouseout(function(){
@@ -20,6 +20,25 @@ $(window).load(function(){
 	//scroller
 	$(".nano").nanoScroller();
 
-	
 
+	// thumbs navigation
+	$('.t_overlay').eq(0).click(function(){
+		$('.viewer').animate({scrollLeft:0},1800);
+	});
+
+	$('.t_overlay').eq(1).click(function(){
+		$('.viewer').animate({scrollLeft:1000},1800);
+	});
+
+	$('.t_overlay').eq(2).click(function(){
+		$('.viewer').animate({scrollLeft:2000},1800);
+	});
+
+	$('.t_overlay').eq(3).click(function(){
+		$('.viewer').animate({scrollLeft:3000},1800);
+	});
+
+	$('.t_overlay').eq(4).click(function(){
+		$('.viewer').animate({scrollLeft:4000},1800);
+	});
 })
